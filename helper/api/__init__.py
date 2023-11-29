16e2c4398831b4893e3b9eb2c54919984e84ce58
erlison61 <erlison.souza@academico.ifpb.edu.br>
2023-11-29 18:44:28 -0300

    adicionado endpoints para professor e aula

--- helper/api/__init__.py
+++ helper/api/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
+from resources.Professor import ProfessorResource
+from resources.Aula import AulaResource
 
 from resources.Index import IndexResource
 
@@ -8,3 +10,5 @@ blueprint = Blueprint('api', __name__)
 api = Api(blueprint, prefix="/api")
 
 api.add_resource(IndexResource, '/')
+api.add_resource(ProfessorResource, '/professores/<int:id>')
+api.add_resource(AulaResource, '/aulas/<int:id>')
