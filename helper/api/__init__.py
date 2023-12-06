1b12f7ee67d3026b6c5e6afa6a86434b71a15d08
rhavymaia <rmg3@cin.ufpe.br>
2023-12-06 15:36:59 -0300

    Atualização nos recursos.

--- helper/api/__init__.py
+++ helper/api/__init__.py
@@ -3,19 +3,19 @@ from flask_restful import Api
 
 from resources.Aula import AulaResource, AulasResource
 from resources.Index import IndexResource
-from resources.Professor import ProfessorResource
-from resources.Sala import SalaResource
+from resources.Professor import ProfessorResource, ProfessoresResource
+from resources.Sala import SalaResource, SalasResource
 
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint, prefix="/api")
 
 api.add_resource(IndexResource, '/')
-api.add_resource(ProfessorResource, '/professores/<int:id>', endpoint='professor_by_id')
-api.add_resource(ProfessorResource, '/professores', endpoint='professors')
+api.add_resource(ProfessorResource, '/professores/<int:id>')
+api.add_resource(ProfessoresResource, '/professores')
 
-api.add_resource(SalaResource, '/sala/<int:id>', endpoint='sala_by_id')
-api.add_resource(SalaResource, '/sala', endpoint='salas')
+api.add_resource(SalaResource, '/sala/<int:id>')
+api.add_resource(SalasResource, '/sala')
 
-api.add_resource(AulaResource, '/aulas/<int:id>', endpoint='aula_by_id')
-api.add_resource(AulasResource, '/aulas', endpoint='aulas')
+api.add_resource(AulaResource, '/aulas/<int:id>')
+api.add_resource(AulasResource, '/aulas')
